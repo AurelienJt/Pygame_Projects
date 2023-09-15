44eb95e9f4b12efcedc9e8e43239126f026da01d
Ornithopter747 <140212267+OrnitOnGithub@users.noreply.github.com>
2023-09-15 22:35:47 +0200

    Created setup script
    This script downloads all dependencies. Look inside for more details. Adding more stuff is super easy.
    
    Also can I have permission to modify? I don't wanna have to make a PR for everything.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Installs all dependencies.
+
+# Run with > sh ./setup.sh
+# If you're not in the Game.Py directory you can either > cd Games.Py and run the command normally or > sh ./PathToGamesPy/Games.Py
+
+pip3 install pygame
+
+echo "installed all dependencies."
