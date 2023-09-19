6261287e2a7c335a320ecef0d14538fd138a7094
AxaQuilPre <112755557+AxaQuilPre@users.noreply.github.com>
2023-09-19 18:06:14 +0200

    Minor spelling change

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Installs all dependencies.
 
 # Run with > sh ./setup.sh
-# If you're not in the Game.Py directory you can either > cd Games.Py and run the command normally or > sh ./PathToGamesPy/Games.Py
+# If you're not in the poroject directory you can either > cd Games.Py and run the command normally or > sh ./PathToGamesPy/Games.Py
 
 pip3 install pygame
 
