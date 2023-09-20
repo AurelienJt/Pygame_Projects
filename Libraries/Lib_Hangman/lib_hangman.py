8c76527dc5a9f55819eb57941407f6bdaa9fdbf6
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2023-09-20 14:21:26 +0200

    recursive fonction + snake case

--- Libraries/Lib_Hangman/lib_hangman.py
+++ Libraries/Lib_Hangman/lib_hangman.py
@@ -1,8 +1,9 @@
 import time
 import os
 
-guessLetter = ""
-gameWord = ""
+guess_letter = ""
+game_word = ""
+total_turns = 0
 
 startDrawing ="""
   +----+
@@ -114,39 +115,68 @@ game_rules = """
 ---------------------- This was made by _N1ghtW0lf ----------------------
 """
 
-def loadingDots():
+def loading_dots():
   print("Drawing")
-  clearTerminal()
+  clear_terminal()
   print("Drawing.")
-  clearTerminal()
+  clear_terminal()
   print("Drawing..")
-  clearTerminal()
+  clear_terminal()
   print("Drawing...")
-  clearTerminal()
+  clear_terminal()
   print("Choosing word")
-  clearTerminal()
+  clear_terminal()
   print("Choosing word.")
-  clearTerminal()
+  clear_terminal()
   print("Choosing word..")
-  clearTerminal()
+  clear_terminal()
   print("Choosing word...")
-  clearTerminal()
+  clear_terminal()
   print("Loading")
-  clearTerminal()
+  clear_terminal()
   print("Loading.")
-  clearTerminal()
+  clear_terminal()
   print("Loading..")
-  clearTerminal()
+  clear_terminal()
   print("Loading...")
-  clearTerminal()
+  clear_terminal()
   time.sleep(0.5)
 
-def clearTerminal():
+def clear_terminal():
     time.sleep(0.5)
     os.system('clear')
 
-def checkGuess(amountOfGuesses):
-  if guessLetter in gameWord:
-    print("- Nice ! You got that one right")
-  else:
-    print("- Bummer, that was wrong...")
+def recursive_check_rules():
+  clear_terminal()
+  try :
+      answer = int(input("\n- Do you know the rules of Hangman?\n\n~Please enter 1 or 2~\n\n[1]: Yes\n[2]: No\n\n"))
+  except ValueError:
+      print("Error. Please enter either 1 or 2")
+
+  if answer == 1 or answer == 2:
+     if answer == 1:
+        pass
+     if answer == 2:
+            clear_terminal()
+
+            print("\n- Here are the rules !\n")
+            print(game_rules, "\n~Press [Enter] to continue~\n")
+            str(input())
+  else :
+      str(input("\nError. Please enter 1 or 2\n\n~Press [Enter] to continue~\n"))
+      recursive_check_rules()
+
+def win():
+    clear_terminal()
+    print ("\n-------------------------------------------------------------------------")
+    print(win_drawing, "\n")
+    print("Good job, you won in", total_turns, "turns !! \nThe word was", game_word, "\n\n")
+    exit()    
+
+def recursive_check_guess():
+    guess_letter = input("\n\nGuess a character : ")
+    if len(guess_letter) > 1:
+        if guess_letter == game_word:
+            win()
+        str(input("\nOnly insert one character please.\n\n~Press [Enter] to continue~\n"))
+        recursive_check_guess()
